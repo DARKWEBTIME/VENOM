434f82f197249ea0af3ca45bf4e74b1147219352
DARKWEBTIME <123661146+DARKWEBTIME@users.noreply.github.com>
2023-01-26 23:30:00 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os, sys
+os.system("pip install telethon")
+os.system("pip install pyfiglet")
+os.system("pip install pytube")
+os.system("pip install aiocron")
+os.system("pip install requests")
+#http://t.me/DARKWEB_TIME
